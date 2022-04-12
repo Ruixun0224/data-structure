5e605f171c5648bd2dad421955b6ada92cb00bfb
ruixun <yuruixun0224@163.com>
2022-04-12 15:47:29 +0800

    finish queue simple function

--- source_code/DS/DS_Learning/stack&queue/queue1.cpp
+++ source_code/DS/DS_Learning/stack&queue/queue1.cpp
@@ -10,5 +10,67 @@ typedef struct {
     int data[MAXSIZE];
     int front;
     int rear;
-}SqQueue;
+} SqQueue;
 
+/**
+ * 初始化队列，头尾指向第一个位置
+ * @param Sq
+ */
+void InitSqQueue(SqQueue &Sq) {
+    Sq.front = Sq.rear = 0;
+}
+
+/**
+ * 判断队列是否为空
+ * @param Sq
+ * @return
+ */
+bool QueueEmpty(SqQueue Sq) {
+    if (Sq.rear == Sq.front)
+        return true;
+    else
+        return false;
+}
+
+/**
+ * 入队操作
+ * @param Sq
+ * @param x
+ * @return
+ */
+bool EnQueue(SqQueue &Sq, int x) {
+    //判断队列中的元素是否已满
+    //浪费一个数据元素
+    if ((Sq.rear + 1) % MAXSIZE == Sq.front)
+        return false;
+    Sq.data[Sq.rear] = x;
+    Sq.rear = (Sq.rear + 1) % MAXSIZE;
+    return true;
+}
+
+/**
+ * 出队操作
+ * @param Sq
+ * @param x
+ * @return
+ */
+bool DeQueue(SqQueue &Sq, int &x) {
+    if (Sq.rear == Sq.front)
+        return false;
+    x = Sq.data[Sq.front];
+    Sq.front = (Sq.front + 1) % MAXSIZE;
+    return true;
+}
+
+/**
+ * 取出队头的元素
+ * @param Sq
+ * @param x
+ * @return
+ */
+bool GetHead(SqQueue Sq, int &x) {
+    if (Sq.rear == Sq.front)
+        return false;
+    x = Sq.data[Sq.front];
+    return true;
+}
--- source_code/DS/DS_Learning/stack&queue/queue2.cpp
+++ source_code/DS/DS_Learning/stack&queue/queue2.cpp
@@ -1,14 +1,15 @@
 //
 // Created by yurui on 2022/4/10.
 //
+#include "stdlib.h"
 
 /**
  * 队结点定义
  */
-typedef struct QNode{
+typedef struct QNode {
     int data;
     struct QNode *next;
-}QNode;
+} QNode;
 
 /**
  * 链队定义
@@ -16,5 +17,56 @@ typedef struct QNode{
 typedef struct {
     QNode *front;
     QNode *rear;
-}LiQueue;
+} LiQueue;
 
+/**
+ * 初始化链队
+ * @param Q
+ */
+void InitQueue(LiQueue &Q) {
+    Q.front = Q.rear = (QNode *) malloc(sizeof(QNode));
+    Q.front->next = NULL;
+}
+
+/**
+ * 判断队列是否为空
+ * @param Q
+ * @return
+ */
+bool QueueEmpty(LiQueue Q) {
+    if (Q.front == Q.rear)
+        return true;
+    else
+        return false;
+}
+
+/**
+ * 新元素入队操作
+ * @param Q
+ * @param x
+ */
+void EnQueue(LiQueue &Q, int x) {
+    QNode *s = (QNode *) malloc(sizeof(QNode));
+    s->data = x;
+    s->next = NULL;
+    Q.rear->next = s;
+    Q.rear = s;
+}
+
+/**
+ * 出队操作
+ * @param Q
+ * @param x
+ * @return
+ */
+bool DeQueue(LiQueue &Q, int &x) {
+    if(Q.front == Q.rear)
+        return false;
+    QNode *p = Q.front->next;
+    x = p->data;
+    Q.front->next = p->next;
+    if(Q.rear == p)
+        Q.rear = Q.front;
+    free(p);
+    return true;
+}
