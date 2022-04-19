8369a2b1e58aef2ccc144f51dcfc30fd6cdb4758
ruixun <yuruixun0224@163.com>
2022-04-19 18:25:29 +0800

    完成二叉树简单遍历

--- source_code/DS/DS_Learning/tree/tree1.cpp
+++ source_code/DS/DS_Learning/tree/tree1.cpp
@@ -3,6 +3,21 @@
 //
 #include "stdio.h"
 
+#define MAXSIZE 10
+
+typedef struct DNode {
+    int data;
+    struct DNode *next;
+} DNode;
+
+typedef struct LinkNode {
+    DNode *front, *rear;
+} LinkNode;
+
+typedef struct {
+    int data[MAXSIZE];
+    int top;
+} Stack;
 /**
  * 二叉树的链式存储
  */
@@ -15,9 +30,9 @@ typedef struct BiTNode {
  * 访问节点操作
  * @param T
  */
-void visit(BiTree T){
-    printf("lchild is %d",T->lchild->data);
-    printf("rchild is %d",T->rchild->data);
+void visit(BiTree T) {
+    printf("lchild is %d", T->lchild->data);
+    printf("rchild is %d", T->rchild->data);
 }
 
 /**
@@ -25,7 +40,7 @@ void visit(BiTree T){
  * @param T
  */
 void PreOrder(BiTree T) {
-    if (T != NULL){
+    if (T != NULL) {
         visit(T);
         PreOrder(T->lchild);
         PreOrder(T->rchild);
@@ -36,8 +51,8 @@ void PreOrder(BiTree T) {
  * 中序遍历
  * @param T
  */
-void InOrder(BiTree T){
-    if(T!=NULL){
+void InOrder(BiTree T) {
+    if (T != NULL) {
         InOrder(T->lchild);
         visit(T);
         InOrder(T->rchild);
@@ -48,10 +63,49 @@ void InOrder(BiTree T){
  * 后序遍历
  * @param T
  */
-void PostOrder(BiTree T){
-    if(T!=NULL){
+void PostOrder(BiTree T) {
+    if (T != NULL) {
         PostOrder(T->lchild);
         PostOrder(T->rchild);
         PostOrder(T);
     }
 }
+
+/**
+ * 初始化栈
+ * @param st
+ */
+void InitStack(Stack &st) {
+    st.top = -1;
+}
+
+/**
+ * 进栈操作
+ * @param st
+ * @param x
+ * @return
+ */
+bool Push(Stack &st, int x) {
+    if (st.top == MAXSIZE - 1)
+        return false;
+    else {
+        st.data[++st.top] = x;
+        return true;
+    }
+}
+
+/**
+ * 出栈操作
+ * @param st
+ * @param x
+ * @return
+ */
+bool Pop(Stack &st, int &x) {
+    if (st.top == -1)
+        return false;
+    else {
+        x = st.data[st.top--];
+        return true;
+    }
+}
+
